1ba8edd6d0bbc55048e676ff3b8f4a1fe73bae3f
oehsaye <samborskioskar@gmail.com>
2018-01-10 19:41:56 +0100

    nowe dane w users.js

---
+++ App/Models/users.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    Bon?ur madam
+
+    <script>
+        console.log("Nasz pierwszy skrypt!");
+    </script>
+</body>
+</html> 
